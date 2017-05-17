533e412b0a4541d1de807cb31681829524c625a5
Sara Honorato <sarahonorato@msn.com>
2017-05-16 20:19:15 -0700

    SearchBar

--- MobileBDG/MobileBDG/Home.xaml.cs
+++ MobileBDG/MobileBDG/Home.xaml.cs
@@ -15,6 +15,59 @@ namespace MobileBDG
         public Home()
         {
             InitializeComponent();
+            Traducao();
+        }
+
+        private void Traducao() {
+            BarraBusca.Placeholder = Resx.Shared.Strings.TextSearchField;
+        }
+
+        private async Task BarraBusca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                ListaHome.BeginRefresh();
+
+                //if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                //    ListaHome.ItemsSource = _container.Employees;
+                //else
+                //    ListaHome.ItemsSource = _container.Employees.Where(i => i.Name.Contains(e.NewTextValue));
+
+                ListaHome.EndRefresh();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+
+        private async Task ListaHome_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                //var selectedItem = (Employee)e.Item;
+
+                //var employeePage = new EmployeePage(selectedItem);
+                //await Navigation.PushAsync(employeePage, true);
+
+                //ListaHome.SelectedItem = null;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+
+        private async Task ListaHome_Refreshing(object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "Ok");
+            }
         }
     }
 }
